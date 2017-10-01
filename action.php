7b94c8cc9443b64967b36cff90e889096e33973e
mossisme <31742728+mossisme@users.noreply.github.com>
2017-10-01 11:24:21 -0400

    Add files via upload

--- action.php
+++ action.php
@@ -1,5 +1,73 @@
 <html>
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Autocomplete - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mootools/1.6.0/mootools-core.min.js"></script>
+<style>
+	p {
+    margin: 1cm 1cm 1cm 1cm;
+    color: "white";
+  }
+   body {
+     font-family: verdana;
+     size: 12;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+li {
+    float: left;
+}
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-family: Verdana;
+}
+li a:hover {
+    background-color: #111;
+}
+</style>
+
+
+
+
+
+</head>
 <body> 
+<ul>
+  <li><a href="http://nootter.com.s3-website-us-east-1.amazonaws.com/">Home</a></li>
+  <li><a href="#about">About</a></li>
+  <li><a href="https://s3.amazonaws.com/nootter.com/organizer.html">Organizer</a></li>
+  <li><a href="https://s3.amazonaws.com/nootter.com/map.html">Map</a></li>
+  <li><a class="active" href="https://s3.amazonaws.com/nootter.com/col.html">COL</a></li>
+  <li><a href="#interviewtips/suggestions">Inteview Tips</a></li>
+</ul>
+
+<style>
+     { margin: 0; padding: 0; }
+    html { 
+        background-color: 
+        /**background-image: url("images/bground.jpeg"); */
+        background-size: cover;
+        background-color: #696969;
+    } 
+    }
+</style>
+
 <?php 
 
 $cities = array('Anniston-Calhoun AL', 'Akron OH', 'Albany GA', 'Albany NY', 'Alexandria LA', 'Amarillo TX', 'Americus GA', 'Ames IA', 'Anchorage AK', 'Anderson SC', 'Appleton WI', 'Ardmore OK', 'Arlington TX', 'Asheville NC', 'Ashland OH', 'Atlanta GA', 'Auburn-Opelika AL', 'Augusta-Aiken GA-SC', 'Austin TX', 'Bakersfield CA', 'Baltimore MD', 'Baton Rouge LA', 'Beaufort SC', 'Beaumont TX', 'Bellingham WA', 'Bergen-Passaic NJ', 'Bethesda-Gaithersburg-Frederick MD', 'Binghamton NY', 'Birmingham AL', 'Bismarck-Mandan ND', 'Blacksburg VA', 'Boise ID', 'Boston MA', 'Bowling Green KY', 'Bozeman MT', 'Bradenton FL', 'Brazoria County TX', 'Brownsville TX', 'Buffalo NY', 'Burlington IA', 'Burlington NC', 'Burlington-Chittenden Co VT', 'Camden SC', 'Cape Coral-Fort Myers FL', 'Carlsbad NM', 'Cedar City UT', 'Cedar Rapids IA', 'Champaign-Urbana IL', 'Chapel Hill NC', 'Charleston WV', 'Charleston-N Charleston SC', 'Charlotte NC', 'Charlottesville VA', 'Chattanooga TN', 'Cheyenne WY', 'Chicago IL', 'Cincinnati OH', 'Clarksburg WV', 'Clarksville TN', 'Cleveland OH', 'Cleveland TN', 'Colorado Springs CO', 'Columbia MO', 'Columbia SC', 'Columbus OH', 'Conroe TX', 'Conway AR', 'Cookeville TN', 'Corpus Christi TX', 'Covington KY', 'Dallas TX', 'Danville IL', 'Dare County NC', 'Davenport-Moline-Rock Is IA-IL', 'Dayton OH', 'Decatur IL', 'Decatur-Hartselle AL', 'Denver CO', 'Des Moines IA', 'Detroit MI', 'Dodge City KS', 'Dothan AL', 'Douglas GA', 'Dover DE', 'Dubuque IA', 'Durham NC', 'Dutchess  NY', 'Dyersburg TN', 'Eau Claire WI', 'Edmond OK', 'El Paso TX', 'Elkhart-Goshen IN', 'Enid OK', 'Erie PA', 'Eugene OR', 'Evansville IN', 'Everett WA', 'Fairbanks AK', 'Fargo-Moorhead ND-MN', 'Farmington NM', 'Fayetteville AR', 'Fayetteville NC', 'Findlay OH', 'Fitchburg-Leominster MA', 'Flagstaff AZ', 'Florence AL', 'Fort Lauderdale FL', 'Fort Smith AR', 'Fort Wayne-Allen County IN', 'Fort Worth TX', 'Framingham-Natick MA', 'Fresno CA', 'Gainesville FL', 'Galesburg IL', 'Garden City KS', 'Glens Falls NY', 'Glenwood Springs CO', 'Grand Junction CO', 'Grand Rapids MI', 'Green Bay WI', 'Greenville NC', 'Greenville SC', 'Gunnison CO', 'Hammond LA', 'Hampton Roads-SE Virginia VA', 'Harlingen TX', 'Harrisburg PA', 'Harrisonburg VA', 'Hartford CT', 'Hastings NE', 'Hattiesburg MS', 'Hays KS', 'Hickory NC', 'Hilton Head Island SC', 'Honolulu HI', 'Hot Springs AR', 'Houston TX', 'Huntsville AL', 'Hutchinson KS', 'Idaho Falls ID', 'Indiana County PA', 'Indianapolis IN', 'Iowa City IA', 'Ithaca NY', 'Jackson MS', 'Jackson-Madison County TN', 'Jacksonville FL', 'Jacksonville NC', 'Janesville WI', 'Jefferson City MO', 'Johnson City TN', 'Johnstown PA', 'Joliet-Will County IL', 'Jonesboro AR', 'Joplin MO', 'Juneau AK', 'Kalamazoo MI', 'Kalispell MT', 'Kansas City MO-KS', 'Kennewick-Richland-Pasco WA', 'Kinston NC', 'Knoxville TN', 'Kodiak AK', 'Lafayette IN', 'Lafayette LA', 'Lake Charles LA', 'Lake Havasu City AZ', 'Lancaster PA', 'Laramie WY', 'Las Cruces NM', 'Las Vegas NV', 'Lawrence KS', 'Lawton OK', 'Lexington KY', 'Lexington-Buena Vista-Rockbridge VA', 'Lima OH', 'Little Rock AR', 'Logan UT', 'Los Alamos NM', 'Los Angeles-Long Beach CA', 'Louisville KY', 'Loveland CO', 'Lubbock TX', 'Lufkin TX', 'Lynchburg VA', 'Manchester NH', 'Manhattan KS', 'Mankato MN', 'Marietta GA', 'Marion-McDowell County NC', 'Marshfield WI', 'Martinsburg-Berkeley County WV', 'Martinsville-Henry County VA', 'Mason City IA', 'McAllen TX', 'Memphis TN', 'Miami-Dade County FL', 'Middlesex-Monmouth NJ', 'Midland TX', 'Milwaukee-Waukesha WI', 'Minneapolis MN', 'Minot ND', 'Missoula MT', 'Mobile AL', 'Monroe LA', 'Montgomery AL', 'Morgantown WV', 'Morristown TN', 'Muncie IN', 'Murfreesboro-Smyrna TN', 'Muskogee OK', 'Myrtle Beach SC', 'Nacogdoches TX', 'Nashville-Franklin TN', 'Nassau County NY', 'New Haven CT', 'Brooklyn NY', 'Manhattan NY', 'Queens NY', 'Newark-Elizabeth NJ', 'Norman OK', 'Oakland CA', 'Odessa TX', 'Oklahoma City OK', 'Olympia WA', 'Omaha NE', 'Orange County CA', 'Orlando FL', 'Paducah KY', 'Palm Springs CA', 'Panama City FL', 'Paris TX', 'Pascagoula MS', 'Peoria IL', 'Philadelphia PA', 'Phoenix AZ', 'Pittsburgh PA', 'Pittsfield MA', 'Plano TX', 'Plattsburgh NY', 'Ponca City OK', 'Portland ME', 'Portland OR', 'Prescott-Prescott Valley AZ', 'Providence RI', 'Pryor Creek OK', 'Pueblo CO', 'Quincy IL', 'Raleigh NC', 'Reno-Sparks NV', 'Richmond IN', 'Richmond VA', 'Rio Rancho NM', 'Riverside City CA', 'Roanoke VA', 'Rochester MN', 'Rochester NY', 'Rockford IL', 'Roswell NM', 'Round Rock TX', 'Sacramento CA', 'Salina KS', 'Salt Lake City UT', 'San Angelo TX', 'San Antonio TX', 'San Diego CA', 'San Francisco CA', 'San Jose CA', 'San Marcos TX', 'Sarasota FL', 'Savannah GA', 'Seattle WA', 'Seguin TX', 'Sheboygan WI', 'Shreveport-Bossier City LA', 'Sierra Vista AZ', 'Sioux Falls SD', 'Slidell Tammany Parish LA', 'South Bend IN', 'Spokane WA', 'Springfield IL', 'Springfield MO', 'St. Cloud MN', 'St. George UT', 'St. Joseph MO', 'St. Louis MO-IL', 'St. Paul MN', 'Stamford CT', 'Staunton-Augusta County VA', 'Stillwater OK', 'Sumter SC', 'Syracuse NY', 'Tacoma WA', 'Tampa FL', 'Temple TX', 'Thomasville-Lexington NC', 'Topeka KS', 'Troy-Miami County OH', 'Truckee-Nevada County CA', 'Tucson AZ', 'Tulsa OK', 'Tupelo MS', 'Tuscaloosa AL', 'Twin Falls ID', 'Tyler TX', 'Valdosta GA', 'Vancouver WA', 'Vero Beach-Indian River FL', 'Waco TX', 'Washington-Arlington-Alexandria DC-VA', 'Waterloo-Cedar Falls IA', 'Wausau WI', 'Weatherford TX', 'Wichita Falls TX', 'Wichita KS', 'Williamsport-Lycoming Co PA', 'Wilmington DE', 'Wilmington NC', 'Winchester VA-WV', 'Winston-Salem NC', 'Wooster OH', 'Yakima WA', 'York County PA', 'Youngstown-Warren OH', 'Yuma AZ'
@@ -9,15 +77,19 @@ $monies = array(91.2, 100.2, 90.1, 108.1, 95.1, 89.5, 88.3, 96.8, 128.4, 91.8, 9
 
 $indexone = array_search($_GET["cityone"], $cities);
 $indextwo = array_search($_GET["citytwo"], $cities);
+echo "<br>";  
+echo "<br>"; 
 
 echo "Considering cost of living and salary, your purchasing power in ", $_GET["cityone"], " is $", round($_GET["moneyone"] / $monies[$indexone] * 100, 2), ", while your purchasing power in ", $_GET["citytwo"], " is $", round($_GET["moneytwo"] / $monies[$indextwo] * 100, 2),  ".";
 
 
 if (round($_GET["moneyone"] / $monies[$indexone] * 100, 2) > round($_GET["moneytwo"] / $monies[$indextwo] * 100, 2)) {
-	echo "  Thus you can purchase more in ", $_GET["cityone"], ".";
+	echo "  Thus you can purchase more in ", $_GET["cityone"], ".<br>";
 } else {
-	echo "  Thus you can purchase more in ", $_GET["citytwo"], ".";
+	echo "  Thus you can purchase more in ", $_GET["citytwo"], ".<br>";
 }
+echo "<br>";  
+echo "<br>";  
 
 if ($_GET["funOption"] == "Burritos") {
 	if (round($_GET["moneyone"] / $monies[$indexone] * 100, 2) > round($_GET["moneytwo"] / $monies[$indextwo] * 100, 2)) {
